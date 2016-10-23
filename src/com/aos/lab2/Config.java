f0c023aa4f01f068a391459506332da6f16f0dcb
nikhil sathe <nikhilss1991@gmail.com>
2016-10-23 04:22:20 +0000

    Config.java edited for proj2

--- src/com/aos/lab2/Config.java
+++ src/com/aos/lab2/Config.java
@@ -1,45 +1,86 @@
 package com.aos.lab2;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Config {
 
-	private int noOfNodes;
-	private List<Node> nodes = new LinkedList<Node>();
-	private Map<Integer, List<Integer>> nodeIdVsPath = new HashMap<Integer, List<Integer>>();
-	private Map<Integer, Node> nodeIdVsNodes = new HashMap<Integer, Node>();
+    private int noOfNodes;
+    private int csExecTime;
+    private int waitTime;
+    private int noOfAttempts;
+    private List<Node> nodes = new LinkedList<Node>();
+    private Map<Integer, List<Integer>> nodeIdVsQuorum = new HashMap<Integer, List<Integer>>();
+    private Map<Integer, Node> nodeIdVsNodes = new HashMap<Integer, Node>();
 
-	public Config(int noOfNodes, List<Node> nodes, Map<Integer, List<Integer>> nodeIdVsPath) {
-		super();
-		this.noOfNodes = noOfNodes;
-		this.nodes = nodes;
-		this.nodeIdVsPath = nodeIdVsPath;
-		populateNodeIdVsNodesMap();
-	}
+    public Config(int noOfNodes,int csExecTime,int waitTime,int noOfAttempts, List<Node> nodes, Map<Integer, List<Integer>> nodeIdVsPath) {
+        super();
+        this.noOfNodes = noOfNodes;
+        this.csExecTime=csExecTime;
+        this.waitTime=waitTime;
+        this.noOfAttempts=noOfAttempts;
+        this.nodes = nodes;
+        this.nodeIdVsQuorum = nodeIdVsPath;
+        populateNodeIdVsNodesMap();
+    }
 
-	private void populateNodeIdVsNodesMap() {
-		for (Node node : nodes) {
-			nodeIdVsNodes.put(node.getNodeId(), node);
-		}
-	}
+    private void populateNodeIdVsNodesMap() {
+        for (Node node : nodes) {
+            nodeIdVsNodes.put(node.getNodeId(), node);
+        }
+    }
 
-	public int getNoOfNodes() {
-		return noOfNodes;
-	}
+    public int getNoOfNodes() {
+        return noOfNodes;
+    }
 
-	public List<Node> getNodes() {
-		return nodes;
-	}
+    public int getCsExecTime() {
+        return csExecTime;
+    }
+    public int getWaitTime() {
+        return waitTime;
+    }
+    public int getNoOfAttempts() {
+        return noOfAttempts;
+    }
 
-	public List<Integer> getNodePathById(Integer id) {
-		return nodeIdVsPath.get(id);
-	}
 
-	public Node getNodeById(Integer id) {
-		return nodeIdVsNodes.get(id);
-	}
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public List<Integer> getNodePathById(Integer id) {
+        return nodeIdVsQuorum.get(id);
+    }
+
+    public Node getNodeById(Integer id) {
+        return nodeIdVsNodes.get(id);
+    }
+
+    public Boolean checkIntersection(){
+        int counter=0;
+        for(Integer i : nodeIdVsQuorum.keySet()){
+            List<Integer> first = new ArrayList<>();
+            first = nodeIdVsQuorum.get(i);
+            int tempc = 1;
+            for(Integer j : nodeIdVsQuorum.keySet()){
+                List<Integer> temp = new ArrayList<>();
+                if(i!=j){
+                    for(int m=0;m<nodeIdVsQuorum.get(j).size();m++)
+                        temp.add(nodeIdVsQuorum.get(j).get(m));
+                    temp.retainAll(first);
+                    if(temp.size()>0)
+                        tempc++;
+                }
+            }
+            if(tempc==noOfNodes){
+                counter++;
+            }
+        }
+        if(counter==noOfNodes)
+            return true;
+        else
+            return false;
+    }
 
 }

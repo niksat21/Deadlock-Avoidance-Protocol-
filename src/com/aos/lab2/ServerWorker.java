d3db2f1510fa1b7a3478982b6b7a29c55a3f37e9
ANKUSH\ankush <ankushraizada@gmail.com>
2016-10-29 16:57:16 -0500

    SCTP Changes

--- src/com/aos/lab2/ServerWorker.java
+++ src/com/aos/lab2/ServerWorker.java
@@ -1,17 +1,27 @@
 package com.aos.lab2;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
-
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import com.sun.nio.sctp.AssociationChangeNotification;
+import com.sun.nio.sctp.HandlerResult;
+import com.sun.nio.sctp.MessageInfo;
+import com.sun.nio.sctp.SctpChannel;
+import com.sun.nio.sctp.SctpServerChannel;
+import com.sun.nio.sctp.ShutdownNotification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.aos.lab2.Server.AssociationHandler;
 import com.google.gson.Gson;
 
 public class ServerWorker implements Runnable {
@@ -21,18 +31,20 @@ public class ServerWorker implements Runnable {
 	private volatile static int result = -1;
 
 	private Logger logger = LogManager.getLogger(ServerWorker.class);
-	private Socket socket;
+	private SctpChannel sc;
 	private Integer nodeId;
 	private Client client;
 	private Integer labelValue;
 	private Config config;
+	private AssociationHandler assocHandler;
 
-	public ServerWorker(Integer nodeId, Socket socket, Client client, Integer labelValue, Config config) {
-		this.socket = socket;
+	public ServerWorker(Integer nodeId, SctpChannel sc, Client client, Integer labelValue, Config config, AssociationHandler assocHandler) {
+		this.sc = sc;
 		this.nodeId = nodeId;
 		this.client = client;
 		this.labelValue = labelValue;
 		this.config = config;
+		this.assocHandler = assocHandler;
 	}
 
 	@Override
@@ -40,70 +52,93 @@ public class ServerWorker implements Runnable {
 		try {
 			// Sleep for sometime so that the other nodes come up.
 			Thread.sleep(8000);
-			DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-			DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-			while (true && !isCompleted) {
-				String json = reader.readLine();
-				Gson gson = new Gson();
-				logger.debug("Received msg:{} ", json);
-				if (json != null) {
-					String parsedJson = json.substring(2);
-					logger.debug("Sanitized msg:{} ", parsedJson);
-					Message msg = gson.fromJson(parsedJson, Message.class);
-					if (msg.getMsgType().equals(MessageType.DATA)) {
-						if (msg.getPath().size() == 0) {
-							logger.info("Output value: {} from node:{} ", msg.getValue(), nodeId);
-							result = msg.getValue();
-							synchronized (isCompleted) {
-								if (!isCompleted())
-									client.broadcastCompletionMsg();
-							}
-						} else {
-							msg.setSource(msg.getDestination());
-							msg.setDestination(msg.getPath().remove(0));
-							msg.setValue(msg.getValue() + labelValue);
-							client.sendMsg(msg);
-						}
-					} else if (msg.getMsgType().equals(MessageType.COMPLETED)) {
-						completedSet.add(msg.getSource());
-						logger.debug("NodeId:{} Completed Set size:{}. CompletedSet:{}", nodeId, completedSet.size(),
-								completedSet);
-
-						// Close socket if it has received the broadcast message
-						// from other nodes.
-						if (checkCompletion())
-							return;
-					} else {
-						logger.error("Unknown message type: {} in the message:{}", msg.getMsgType(), json);
-					}
-				} else if (checkCompletion()) {
-					return;
-				} else {
-
-					logger.debug("Sleeping for 3 seconds as there is nothing to read in socket from host:{}",
-							socket.getRemoteSocketAddress());
-					Thread.sleep(3000);
-				}
+//			DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+//			DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());			
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+			
+			
+            
+			while (true /*&& !isCompleted*/) {
+			ByteBuffer buf = ByteBuffer.allocateDirect(500000);
+            MessageInfo messageInfo = sc.receive(buf, System.out, assocHandler);
+            buf.flip();
+            byte[] data = new byte[buf.remaining()];
+            buf.get(data, 0, data.length);
+            ByteArrayInputStream bis = new  ByteArrayInputStream(data);
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            Message msg = (Message)ois.readObject();
+            
+            //clearing buffer
+            buf.clear();
+//				String json = reader.readLine();
+//				Gson gson = new Gson();
+//				logger.debug("Received msg:{} ", json);
+//				if (json != null) {
+//					String parsedJson = json.substring(2);
+//					logger.debug("Sanitized msg:{} ", parsedJson);
+//					Message msg = gson.fromJson(parsedJson, Message.class);
+//					if (msg.getMsgType().equals(MessageType.DATA)) {
+//						if (msg.getPath().size() == 0) {
+//							logger.info("Output value: {} from node:{} ", msg.getValue(), nodeId);
+//							result = msg.getValue();
+//							synchronized (isCompleted) {
+//								if (!isCompleted())
+//									client.broadcastCompletionMsg();
+//							}
+//						} else {
+//							msg.setSource(msg.getDestination());
+//							msg.setDestination(msg.getPath().remove(0));
+//							msg.setValue(msg.getValue() + labelValue);
+//							client.sendMsg(msg);
+//						}
+//					} else if (msg.getMsgType().equals(MessageType.COMPLETED)) {
+//						completedSet.add(msg.getSource());
+//						logger.debug("NodeId:{} Completed Set size:{}. CompletedSet:{}", nodeId, completedSet.size(),
+//								completedSet);
+//
+//						// Close socket if it has received the broadcast message
+//						// from other nodes.
+//						if (checkCompletion())
+//							return;
+//					} else {
+//						logger.error("Unknown message type: {} in the message:{}", msg.getMsgType(), json);
+//					}
+//				} else if (checkCompletion()) {
+//					return;
+//				} else {
+//
+//					logger.debug("Sleeping for 3 seconds as there is nothing to read in socket from host:{}",
+//							socket.getRemoteSocketAddress());
+//					Thread.sleep(3000);
+//				}
 			}
 		} catch (Exception e) {
 			logger.error("Exception in Server Worker thread", e);
+//			try {
+//        	 	if(ois!=null)
+//             	ois.close();
+//             	if(bis!=null)
+//     			bis.close();
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
 		}
 	}
 
-	private boolean checkCompletion() throws IOException {
-		synchronized (isCompleted) {
-			if (completedSet.size() == config.getNoOfNodes() - 1 && result != -1) {
-				logger.debug("Closing socket between local host:{} and remote host:{}",
-						socket.getLocalAddress().getHostName(), socket.getRemoteSocketAddress());
-				socket.close();
-				client.closeSockets();
-				isCompleted = true;
-				return true;
-			}
-			return false;
-		}
-	}
+//	private boolean checkCompletion() throws IOException {
+//		synchronized (isCompleted) {
+//			if (completedSet.size() == config.getNoOfNodes() - 1 && result != -1) {
+//				logger.debug("Closing socket between local host:{} and remote host:{}"/*,
+//						socket.getLocalAddress().getHostName(), socket.getRemoteSocketAddress()*/);
+//				socket.close();
+//				client.closeSockets();
+//				isCompleted = true;
+//				return true;
+//			}
+//			return false;
+//		}
+//	}
 
 	public static boolean isCompleted() {
 		return isCompleted;

5d70c01a594539005830c8bd0832f6ea97a6ad61
ANKUSH\ankush <ankushraizada@gmail.com>
2016-10-29 16:58:49 -0500

    SCTP Changes

--- src/com/aos/lab2/Client.java
+++ src/com/aos/lab2/Client.java
@@ -1,13 +1,43 @@
 package com.aos.lab2;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.nio.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
+import java.text.SimpleDateFormat;
+import java.nio.channels.spi.AbstractSelectableChannel;
+import com.sun.nio.sctp.MessageInfo;
+import com.sun.nio.sctp.SctpChannel;
+import com.sun.nio.sctp.SctpServerChannel;
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import com.sun.nio.sctp.AssociationChangeNotification;
+import com.sun.nio.sctp.ShutdownNotification;
+import com.sun.nio.sctp.HandlerResult;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,7 +46,7 @@ import com.google.gson.Gson;
 
 public class Client implements Runnable {
 
-	private Map<Integer, Socket> nodeVsSocket = new HashMap<Integer, Socket>();
+	private Map<Integer, SocketAddress> nodeVsSocket = new HashMap<Integer, SocketAddress>();
 	private Logger logger = LogManager.getLogger(Client.class);
 	private String nodeHostname = null;
 	private int labelValue;
@@ -37,13 +67,12 @@ public class Client implements Runnable {
 				if (node.getNodeId().equals(nodeId))
 					continue;
 				logger.debug("Trying to create connection with host:{} port:{}", node.getHostname(), node.getPort());
-				Socket socket = new Socket(node.getHostname(), node.getPort());
-				nodeVsSocket.put(node.getNodeId(), socket);
+				//Socket socket = new Socket(node.getHostname(), node.getPort());
+				SocketAddress socketAddress = new InetSocketAddress(node.getHostname(), node.getPort());
+				nodeVsSocket.put(node.getNodeId(), socketAddress);
 				logger.debug("Successfully created socket connection to host:{} from:{} ", node.getHostname(),
 						nodeHostname);
-			} catch (UnknownHostException e) {
-				logger.error(e);
-			} catch (IOException e) {
+			} catch (Exception e) {
 				logger.error(e);
 			}
 		}
@@ -52,13 +81,12 @@ public class Client implements Runnable {
 
 	public void closeSockets() {
 		try {
-			for (Entry<Integer, Socket> entry : nodeVsSocket.entrySet()) {
-				Socket socket = entry.getValue();
-				logger.debug("Closing socket between local host:{} and remote host:{}", socket.getLocalSocketAddress(),
-						socket.getRemoteSocketAddress());
-				socket.close();
+			for (Entry<Integer, SocketAddress> entry : nodeVsSocket.entrySet()) {
+				SocketAddress socketAddress = entry.getValue();
+				logger.debug("Closing socket between local host:{} and remote host:{}");
+				//socketAddress.close();
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			logger.error("Problem with closing the socket.", e);
 		}
 	}
@@ -78,28 +106,52 @@ public class Client implements Runnable {
 	}
 
 	public void sendMsg(Message msg) {
-		Socket socket = nodeVsSocket.get(msg.getDestination());
+		//Socket socket = nodeVsSocket.get(msg.getDestination());
+		SocketAddress socketAddress = nodeVsSocket.get(msg.getDestination());
 		Gson gson = new Gson();
 		String json = gson.toJson(msg);
 		while (true) {
 			try {
-				if (socket != null) {
-					logger.debug("Trying to send msg:{} from nodeId:{}", json, nodeId);
-					DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-					outputStream.writeUTF(json + "\n");
-					logger.debug("Successfully placed msg: {} in the stream to host:{}", json,
-							socket.getRemoteSocketAddress());
-					break;
-				} else {
-					logger.error(
-							"Unable to find socket for msg:{} destination nodeId:{}. SocketMap: {} Retry after 5 seconds",
-							json, msg.getDestination(), nodeVsSocket);
-					Thread.sleep(5000);
-				}
-			} catch (Exception e) {
-				logger.error("Exception in client thread", e);
-			}
-		}
+//				if (socket != null) {
+//					logger.debug("Trying to send msg:{} from nodeId:{}", json, nodeId);
+//					DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+//					outputStream.writeUTF(json + "\n");
+//					logger.debug("Successfully placed msg: {} in the stream to host:{}", json,
+//							socket.getRemoteSocketAddress());
+//					break;
+//				} else {
+//					logger.error(
+//							"Unable to find socket for msg:{} destination nodeId:{}. SocketMap: {} Retry after 5 seconds",
+//							json, msg.getDestination(), nodeVsSocket);
+//					Thread.sleep(5000);
+//				}
+//			} catch (Exception e) {
+//				logger.error("Exception in client thread", e);
+//			}
+//		}
+		SctpChannel sctpChannel = SctpChannel.open();
+		sctpChannel.connect(socketAddress);
+				
+		MessageInfo messageInfo = MessageInfo.createOutgoing(socketAddress, 0);
+		ByteBuffer buf = ByteBuffer.allocateDirect(500000);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(msg);
+		oos.flush();
+		buf.put(bos.toByteArray());
+		buf.flip();
+		sctpChannel.send(buf, messageInfo);
+		
+		bos.close();
+		buf.clear();
+		//return true;
+	}catch(Exception e){
+		logger.warn("Exception in Send()"+ e);
+		//System.err.println(e);
+		e.printStackTrace();
+		//return false;
+	}
+	}
 	}
 
 	private void initiateMsg() {
--- src/com/aos/lab2/Server.java
+++ src/com/aos/lab2/Server.java
@@ -1,10 +1,18 @@
 package com.aos.lab2;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import com.sun.nio.sctp.AssociationChangeNotification;
+import com.sun.nio.sctp.HandlerResult;
+import com.sun.nio.sctp.MessageInfo;
+import com.sun.nio.sctp.SctpChannel;
+import com.sun.nio.sctp.SctpServerChannel;
+import com.sun.nio.sctp.ShutdownNotification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,34 +36,57 @@ public class Server implements Runnable {
 	public void run() {
 		try {
 			listenForConnections();
-		} catch (UnknownHostException e) {
-			logger.error(e);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			logger.error(e);
 		}
-
 	}
 
 	public void setClientHandler(Client client) {
 		this.client = client;
 	}
 
-	private void listenForConnections() throws UnknownHostException, IOException {
-		ServerSocket socket = new ServerSocket(port);
+	private void listenForConnections() throws /*UnknownHostException, IOException*/Exception {
+		//ServerSocket socket = new ServerSocket(port);
+		SctpServerChannel ssc = SctpServerChannel.open();
+		InetSocketAddress serverAddr = new InetSocketAddress(port);
+		ssc.bind(serverAddr);
+		AssociationHandler assocHandler = new AssociationHandler();
 		try {
 			while (true) {
-				logger.debug("Listening for connection on hostname:{} port:{}",
-						socket.getInetAddress().getHostAddress(), socket.getLocalPort());
-				Socket sock = socket.accept();
-				ServerWorker worker = new ServerWorker(nodeId, sock, client, labelValue, config);
+				logger.debug("Listening for connection on hostname:{} port:{}"/*,
+						socket.getInetAddress().getHostAddress(), socket.getLocalPort()*/);
+				//Socket sock = socket.accept();
+				SctpChannel sc = ssc.accept();
+				ServerWorker worker = new ServerWorker(nodeId, sc, client, labelValue, config, assocHandler);
 				logger.debug("Created server worker");
 				Thread workerThread = new Thread(worker);
 				logger.debug("Created server worker thread");
 				workerThread.start();
 			}
 		} finally {
-			socket.close();
+			ssc.close();
 		}
 	}
+	
+	static class AssociationHandler extends AbstractNotificationHandler<PrintStream>
+	{
+	   public HandlerResult handleNotification(AssociationChangeNotification not,
+	                                           PrintStream stream) {
+	       if (not.event().equals(AssociationChangeNotification.AssocChangeEvent.COMM_UP)) {
+	           int outbound = not.association().maxOutboundStreams();
+	           int inbound = not.association().maxInboundStreams();
+	           //stream.printf("New association setup with %d outbound streams" +
+	            //             ", and %d inbound streams.\n", outbound, inbound);
+	       }
 
+	       return HandlerResult.CONTINUE;
+	   }
+
+	   public HandlerResult handleNotification(ShutdownNotification not,
+	                                           PrintStream stream) {
+	       //stream.printf("The association has been shutdown.\n");
+	       return HandlerResult.RETURN;
+	   }
+	}
+	
 }
